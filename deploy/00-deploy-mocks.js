d07a5db2b4fa42795c8c3a11a7ee554ba0ec6cab
Fiat Bruno <fiatbruno10@gmail.com>
2023-02-18 07:59:53 +0200

    Deploy script for dynamicSvgNft

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,6 +1,9 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
+const DECIMALS = "18"
+const INITIAL_PRICE = ethers.utils.parseUnits("2000", "ether")
+
 const BASE_FEE = ethers.utils.parseEther("0.25")
 const GAS_PRICE_LINK = 1e9
 args = [BASE_FEE, GAS_PRICE_LINK]
@@ -16,6 +19,11 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
             from: deployer,
             log: true,
             args: args,
+        })
+        await deploy("MockV3Aggregator", {
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_PRICE],
         })
         log("✨ Mocks Deployed!")
         log("-----------------------------------------------------------")
